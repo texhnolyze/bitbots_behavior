8eb3c8eb31eb2b69c75a5ba142e7a74ee2bbc46b
texhnolyze <joegriepen@gmail.com>
2023-12-20 15:41:22 +0100

    feat(logging): setup DEBUG logging of all evaluated states
    to be able to find out, which state has which score

--- bitbots_body_behavior/bitbots_body_behavior/evaluation/evaluator.py
+++ bitbots_body_behavior/bitbots_body_behavior/evaluation/evaluator.py
@@ -1,10 +1,14 @@
 from typing import Iterable, Optional, Tuple
 
+import rclpy
+
 from bitbots_body_behavior.actions import Action
 from bitbots_body_behavior.state.state import State
 
 Evaluation = Tuple[Action, State, list[State]]
 EvaluationResult = Tuple[Action, Optional[State], float]
+
+logger = rclpy.logging.get_logger("bitbots_body_behavior")
 
 
 class Evaluator:
@@ -25,7 +29,12 @@ class ActionEvaluator(Evaluator):
     def evaluate_new_states(evaluation: Evaluation) -> Iterable[EvaluationResult]:
         action, current_state, new_states = evaluation
 
+        def evaluate_and_log(new_state: State) -> float:
+            utility = action.evaluate(current_state, new_state)
+            logger.debug(f"({action}, {new_state}) utility: {utility}")
+            return (action, new_state, utility)
+
         return map(
-            lambda state: (action, state, action.evaluate(current_state, state)),
+            evaluate_and_log,
             new_states,
         )
